e110250f7389b15e1710cd42c0e65930f040800a
purujawale <purujawale123@gmail.com>
2021-06-29 22:42:18 +0530

    composer installation

---
+++ vendor/composer/installed.php
@@ -0,0 +1,110 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '339e87e3de39fb647d66119078b2557454867e47',
+    'name' => 'snowdogapps/dev-test',
+  ),
+  'versions' => 
+  array (
+    'container-interop/container-interop' => 
+    array (
+      'pretty_version' => '1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fc08354828f8fd3245f77a66b9e23a6bca48297e',
+    ),
+    'container-interop/container-interop-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '^1.0',
+      ),
+    ),
+    'mnapoli/php-di' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'mnapoli/silly' => 
+    array (
+      'pretty_version' => '1.3.0',
+      'version' => '1.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6b3fbe09e1e628233ad06aa1db6a0ce2991c0ccf',
+    ),
+    'nikic/fast-route' => 
+    array (
+      'pretty_version' => 'v1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '79843dce62ac52e9b628e73d5f1264cad10c65a6',
+    ),
+    'php-di/invoker' => 
+    array (
+      'pretty_version' => '1.3.2',
+      'version' => '1.3.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4140c38ca17268adc500b1e5c85c4055e48608d2',
+    ),
+    'php-di/php-di' => 
+    array (
+      'pretty_version' => '5.3.0',
+      'version' => '5.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '854a6d8f54e2146f0a34f0a28f0adea688b634a3',
+    ),
+    'php-di/phpdoc-reader' => 
+    array (
+      'pretty_version' => '2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '83f5ead159defccfa8e7092e5b6c1c533b326d68',
+    ),
+    'snowdogapps/dev-test' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '339e87e3de39fb647d66119078b2557454867e47',
+    ),
+    'symfony/console' => 
+    array (
+      'pretty_version' => 'v3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '64a4d43b045f07055bb197650159769604cb2a92',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.2.0',
+      'version' => '1.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dff51f72b0706335131b00a7f49606168c582594',
+    ),
+  ),
+);
